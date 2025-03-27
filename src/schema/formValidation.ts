460bec780d73be3bc147264514350a2185267169
navasnoozy <166505582+navasnoozy@users.noreply.github.com>
2025-03-27 15:35:46 +0530

    spelling correction

--- src/schema/formValidation.ts
+++ src/schema/formValidation.ts
@@ -8,7 +8,7 @@ export const formSchema = z.object ({
     email: z.string().min(1,'Email is requied').regex(emailRegex, 'Enter a valid email'),
     hobbies : z.string().array().min(1,'select atleast one').max(2,'No more than two'),
     gender : z.enum(['male','female','other'],{message:'No ? what kind of gender it is?'}),
-    skills: z.string().array().min(1,'Select at least one skill').max(2,'A man with more than 2 skil is overqualified'),
+    skills: z.string().array().min(1,'Select at least one skill').max(2,'A man with more than 2 skill is overqualified'),
     DateTime : z.string().optional()
 });
 
